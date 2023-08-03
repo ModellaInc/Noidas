a05ada5626f510137b0360a716342fa5e6772fcc
ModellaInc <141292838+ModellaInc@users.noreply.github.com>
2023-08-03 18:00:24 +1000

    Add files via upload

---
+++ old/public/app/index.js
@@ -0,0 +1,45 @@
+/*
+	Javascript file for home page, nothing else.
+*/
+function create_post_button_notification(type, text, body) {
+	new Notify({
+		status: type,
+		title: text,
+		text: body,
+		effect: 'fade',
+		speed: 300,
+		customClass: null,
+		customIcon: null,
+		showIcon: true,
+		showCloseButton: true,
+		autoclose: true,
+		autotimeout: 1500,
+		gap: 20,
+		distance: 20,
+		type: 1,
+		position: 'right top'
+	})
+}
+function create_post_button() {
+	const post_content = document.getElementById("create-post-input").value
+	if (post_content != "" && post_content != null) {
+		fetch("/post")
+			.then(function(res) {
+				if (res.status == 201 || res.status == 200) {
+					create_post_button_notification("success", "Posted", "Your post has been posted.")
+				} else {
+					create_post_button_notification("error", "Failed", `Failed to post: http error code: ${res.status}`)
+				}
+			}
+			)
+			.then().catch(function(err) {
+				console.log(err)
+			})
+		
+	} else {create_post_button_notification("error", "Error", "You can't post nothing!")}
+}
+function onload() {
+	window.scrollTo(0, 0);
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
